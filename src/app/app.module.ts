aa62aef201d647c0f995fd8667e04433ce7463fb
Valentine Mapaura <valmapaura@gmail.com>
2023-05-16 13:19:45 +0200

    Added pages and firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { SearchComponent } from './search/search.component';
+import { HomeComponent } from './home/home.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    HomeComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'findmyidzw',
+    appId: '1:542091031714:web:4c34662f4135c1c49013fb',
+    storageBucket: 'findmyidzw.appspot.com',
+    apiKey: 'AIzaSyBaQNv5TWwBBAm3tAbeTfqrNB92YWsNR8c',
+    authDomain: 'findmyidzw.firebaseapp.com',
+    messagingSenderId: '542091031714',
+  },
   production: true
 };
